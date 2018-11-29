0256e074d70a0fc9f6dd41bd3518b216c3dd1500
jeongmin-seo <ya4615@naver.com>
2018-11-30 03:41:14 +0900

    add draw point
    draw point when click left mouse

--- annotation_tool/main.py
+++ annotation_tool/main.py
@@ -4,6 +4,7 @@ import os
 import re
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 
 
 class App(QWidget):
@@ -15,24 +16,22 @@ class App(QWidget):
         self.top = 10
         self.width = 640
         self.height = 640
+        self.lb_size = 500
         self.curdir = None
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-
         self.loadImageDialog()
-        # self.openFileNameDialog()
-        # self.openFileNamesDialog()
-        # self.saveFileDialog()
+        self.setMouseTracking(False)
 
         self.show()
 
     def loadImageDialog(self):
         self.label = QLabel("", self)
         self.label.move(20, 20)
-        self.label.resize(450, 450)
+        self.label.resize(self.lb_size, self.lb_size)
 
         load_btn = QPushButton("Load", self)
         load_btn.move(20, 600)
@@ -50,35 +49,37 @@ class App(QWidget):
         file_list = sorted(os.listdir(self.curdir))
         p = re.compile(".*[.](?=png$|jpg$|jpeg$|PNG$|JPG$|JPEG$).*$")
 
-        #return [os.path.join(self.curdir, file_name) for file_name in file_list if p.search(file_name)]
         return [self.curdir + '/' + file_name for file_name in file_list if p.search(file_name)]
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-                                                  "All Files (*);;Image Files (*.jpg, *.jpeg, *.png, *JPG)", options=options)
+                                                  "All Files (*);;Image Files (*.jpg, *.jpeg, *.png, *.JPG)", options=options)
         self.curdir = os.path.dirname(fileName)
 
         if fileName:
             self.curdir_file_list = self.load_file_list_curdir()
-            print(self.curdir_file_list)
-            print(fileName)
-            self.label.setPixmap(QPixmap(fileName))
             self.idx = self.curdir_file_list.index(fileName)
             self.num_file = len(self.curdir_file_list)
+            self.load_image()
 
     def clicked_next(self):
         if self.curdir:
             self.idx = self.check_minmax(self.idx + 1)
-            self.label.setPixmap(QPixmap(self.curdir_file_list[self.idx]))
-            print(self.curdir_file_list[self.idx])
+            self.load_image()
 
     def clicked_prev(self):
         if self.curdir:
             self.idx = self.check_minmax(self.idx - 1)
-            self.label.setPixmap(QPixmap(self.curdir_file_list[self.idx]))
-            print(self.curdir_file_list[self.idx])
+            self.load_image()
+
+    def load_image(self):
+        print(self.curdir_file_list[self.idx])
+        pixmap = QPixmap(self.curdir_file_list[self.idx])
+        self.ratio = [float(pixmap.width()/self.lb_size), float(pixmap.height()/self.lb_size)]
+        self.org_pixmap = pixmap.scaled(self.lb_size, self.lb_size)
+        self.label.setPixmap(self.org_pixmap)
 
     def check_minmax(self, _idx):
         if _idx < 0:
@@ -87,6 +88,20 @@ class App(QWidget):
             return self.num_file-1
         else:
             return _idx
+
+    def mousePressEvent(self, e):
+        if e.buttons() & Qt.LeftButton and self.curdir:
+
+            print(e.x(), e.y())
+            self.label.setPixmap(self.org_pixmap)
+            painter = QPainter(self.label.pixmap())
+
+            pen = QPen(Qt.red)
+            pen.setWidth(5)
+            painter.setPen(pen)
+            painter.drawPoint(e.x()-20, e.y()-20)
+
+        self.label.update()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
