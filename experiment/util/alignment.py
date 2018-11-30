881bd123fa330a8dd3118f3db0348bce6de395a1
jeongmin <ya4615@naver.com>
2018-12-01 01:44:30 +0900

    add alignment structure

--- experiment/util/alignment.py
+++ experiment/util/alignment.py
@@ -1,4 +1,30 @@
-##################################################
-#
-##################################################
+"""####################### import requirement library ########################"""
+
+import os
+import re
 import cv2
+
+
+class Alignmentor:
+    def __init__(self, image_list, save_root):
+        self.image_list = image_list
+        self.save_root = save_root
+
+    def get_annotation_line(self):
+        pass
+
+    def crop(self):
+        pass
+
+    def get_ratio(self):
+        pass
+
+    def save_aligment_image(self):
+        pass
+
+    def find_threshold_pixel(self):
+        pass
+
+#  A  A
+# (‘ㅅ‘=)
+# J.M.Seo
